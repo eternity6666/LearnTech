4bd6587bc3b35458cbfc225ef051770581bf9ea7
eternity6666 <eternity6666@qq.com>
2025-04-18 14:41:00 +0800

    feat(Apple): init projects

---
+++ Apple/Apple/AppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleApp.swift
+//  Apple
+//
+//  Created by Y1616 on 2025/4/18.
+//
+
+import SwiftUI
+
+@main
+struct AppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
