3001f7bdedc97a53f89a53aa5c00239812df9ac8
eternity6666 <eternity6666@qq.com>
2025-05-17 18:06:16 +0800

    feat(KMP): init project
    update

---
+++ KMP/composeApp/src/desktopMain/kotlin/com/yzh/Main.kt
@@ -0,0 +1,13 @@
+package com.yzh
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "LearnKotlin",
+    ) {
+        App()
+    }
+}
---
+++ KMP/settings.gradle.kts
@@ -0,0 +1,41 @@
+rootProject.name = "KMP"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        maven {
+            url = uri("https://mirrors.tencent.com/nexus/repository/maven-public")
+        }
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        maven {
+            url = uri("https://mirrors.tencent.com/nexus/repository/maven-public")
+        }
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
+}
+
+include(":composeApp")
