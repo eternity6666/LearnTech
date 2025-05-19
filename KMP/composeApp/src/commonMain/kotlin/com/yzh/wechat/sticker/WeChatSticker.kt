5070caf501b1b27e2e960e4a8d591ebe10adf31b
eternity6666 <eternity6666@qq.com>
2025-05-19 10:48:25 +0800

    feat(KMP): update

--- KMP/composeApp/src/commonMain/kotlin/com/yzh/wechat/sticker/WeChatSticker.kt
+++ KMP/composeApp/src/commonMain/kotlin/com/yzh/wechat/sticker/WeChatSticker.kt
@@ -10,6 +10,7 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.get
+import io.ktor.serialization.kotlinx.json.json
 import kotlinx.coroutines.launch
 import kotlinx.serialization.Serializable
 
