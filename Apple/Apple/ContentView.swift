1adac2a3ddeacb3d273f643084c06247eea6ccbc
eternity6666 <eternity6666@qq.com>
2025-05-28 23:48:04 +0800

    feat(Apple): update

--- Apple/Apple/ContentView.swift
+++ Apple/Apple/ContentView.swift
@@ -11,7 +11,8 @@ struct ContentView: View {
     var body: some View {
         VStack {
 //            AnimatedSineWaveDemo()
-            ColorTextView()
+//            ColorTextView()
+            TopBottomTextView()
         }
         .padding()
     }
