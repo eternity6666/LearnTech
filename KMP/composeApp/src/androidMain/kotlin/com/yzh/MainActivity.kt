03df9aaed91bbf129020c29fe07e603194f71819
eternity6666 <eternity6666@qq.com>
2025-05-28 13:19:39 +0800

    update

--- KMP/composeApp/src/androidMain/kotlin/com/yzh/MainActivity.kt
+++ KMP/composeApp/src/androidMain/kotlin/com/yzh/MainActivity.kt
@@ -1,64 +1,98 @@
 package com.yzh
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.lifecycleScope
+import androidx.core.os.postDelayed
+import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import kotlinx.coroutines.withTimeout
 import kotlinx.coroutines.withTimeoutOrNull
-import kotlin.system.measureTimeMillis
+import kotlin.time.measureTimedValue
 
 class MainActivity : ComponentActivity() {
+    val mainScope = MainScope()
+    val mainScope2 = MainScope()
+    private var job: Deferred<Unit>? = null
+    val now by lazy {
+        System.currentTimeMillis()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-
         setContent {
             App()
         }
-        testWithTimeOut()
+        val handler = Handler(Looper.getMainLooper())
+        now
+        handler.postDelayed(1000L) {
+            testWithTimeOut()
+        }
+        handler.postDelayed(3000L) {
+            loadUrl()
+        }
+    }
+
+    private fun print(msg: String) {
+        println("用时=${System.currentTimeMillis() - now} $msg")
     }
 
     private fun testWithTimeOut() {
-        lifecycleScope.launch {
-            val now = System.currentTimeMillis()
-            fun useTime(): Long {
-                return System.currentTimeMillis() - now
-            }
-            fun print(msg: String) {
-                println("baron ${useTime()} $msg")
-            }
-            val job = async {
-                print("before withContext")
+        var calcResult = ""
+        mainScope.launch {
+            job = async {
+                print("计算开始")
                 withContext(Dispatchers.IO) {
-                    print("before delay")
-                    delay(1000L)
-                    "计算完成".also {
-                        print("after delay")
-                    }
+                    delay(2490L)
+                    calcResult = "计算结果"
                 }.also {
-                    print("after withContext")
+                    print("计算完成")
+                }
+                job = null
+            }
+            job?.await()
+        }
+
+        mainScope2.launch {
+            async {
+                withContext(Dispatchers.IO) {
+                    print("其他耗时任务开始")
+                    delay(6000L)
+                    print("其他耗时任务结束")
                 }
             }
-            delay(500L)
-            val time = measureTimeMillis {
-                val result = withTimeoutOrNull(500L) {
-                    print("before await")
-                    job.await().also {
-                        print("after await")
-                    }
-                }
-                print("result=$result")
+        }
+    }
+
+    private fun loadUrl() {
+        mainScope.launch {
+            measureTimedValue {
+                print("等待开始")
+                waitResult()
+            }.also {
+                print("等待耗时:${it.duration.inWholeMilliseconds}")
             }
-            print("time=$time")
+        }
+    }
+
+    private suspend fun waitResult() {
+        val job = this.job ?: return
+        val result = withTimeoutOrNull(500L) {
+            job.await()
+        }
+        this.job = null
+        if (result == null) {
+            print("等待超时")
+        } else {
+            print("等待未超时")
         }
     }
 }
