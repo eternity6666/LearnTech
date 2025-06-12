e20b26ab7b0914584490511dba003022560c520a
eternity6666 <eternity6666@qq.com>
2025-06-12 17:53:01 +0800

    feat(Apple): widget

--- Apple/Apple/UIDevice+Ext.swift
+++ Apple/Apple/UIDevice+Ext.swift
@@ -5,3 +5,23 @@
 //  Created by Y1616 on 2025/6/12.
 //
 
+import SwiftUI
+
+@available(iOS 2.0, *)
+extension UIDevice.BatteryState {
+    var name: String {
+        switch self {
+        case .unknown:
+            "未知"
+        case .unplugged:
+            "未充电"
+        case .charging:
+            "充电中"
+        case .full:
+            "已充满"
+        @unknown default:
+            "未知类型\(self.rawValue)"
+        }
+    }
+    
+}
