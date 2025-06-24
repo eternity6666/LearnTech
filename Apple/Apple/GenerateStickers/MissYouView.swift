92bb091e49a8178b870cc96a80b31407913fc280
eternity6666 <eternity6666@qq.com>
2025-06-24 23:44:58 +0800

    feat(Apple): update

--- Apple/Apple/GenerateStickers/MissYouView.swift
+++ Apple/Apple/GenerateStickers/MissYouView.swift
@@ -12,6 +12,9 @@ struct MissYouViewDemo: View {
     var body: some View {
         VStack {
             MissYouView(config: self.config)
+            PNG(text: "想你的风还是吹到了\n呼伦贝尔", size: .init(width: 750, height: 400), config: config)
+                .scaleEffect(200 / 750)
+                .frame(width: 200, height: 150)
             Button {
                 output()
             } label: {
@@ -24,33 +27,50 @@ struct MissYouViewDemo: View {
     private var config: MissYouView.Config {
         [
             .init(
-                titleColor: Color(47, 79, 79),
-                titleBgColor: Color(245, 245, 220)
-            ),
-            .init(
-                titleColor: Color(63, 112, 77),
-                titleBgColor: Color(176, 224, 230)
-            ),
-            .init(
-                titleColor: Color(255, 255, 255),
-                titleBgColor: Color(44, 62, 80)
-            ),
-            .init(
-                titleColor: Color(253, 253, 150),
-                titleBgColor: Color(85, 107, 47)
-            ),
+                titleColor: Color(245, 245, 245),
+                titleBgColor: Color(55, 113, 187)
+            )
+//            .init(
+//                titleColor: Color(47, 79, 79),
+//                titleBgColor: Color(245, 245, 220)
+//            ),
+//            .init(
+//                titleColor: Color(63, 112, 77),
+//                titleBgColor: Color(176, 224, 230)
+//            ),
+//            .init(
+//                titleColor: Color(255, 255, 255),
+//                titleBgColor: Color(44, 62, 80)
+//            ),
+//            .init(
+//                titleColor: Color(253, 253, 150),
+//                titleBgColor: Color(85, 107, 47)
+//            ),
         ].randomElement() ?? .init()
     }
 
     private func output() {
         Task {
-            if let dirUrl = TopBottomViewModel.dirUrl(title: "路牌表情包") {
+            if let dirUrl = TopBottomViewModel.dirUrl(title: "想你的风表情包") {
+                [
+                    "想你的风吹到了这里",
+                    "我在这里很想你"
+                ].forEach { text in
+                    let url = dirUrl.appendingPathComponent(text)
+                    try? TopBottomViewModel.createDirIfNeed(url)
+                    outputPNG(
+                        url: url,
+                        text: text,
+                        size: .init(width: 60, height: 60),
+                        config: config
+                    )
+                }
                 DataSet.country.forEach { (first, second) in
                     let url = dirUrl.appendingPathComponent("\(first)")
                     try? TopBottomViewModel.createDirIfNeed(url)
                     let config = self.config
-                    outputPNG(url: url, text: first, size: .init(width: 500, height: 500), config: config)
-                    outputPNG(url: url, text: "我在\(first)很想你", size: .init(width: 750, height: 400), config: config)
+                    outputPNG(url: url, text: "想你的风吹到了\n\(first)", size: .init(width: 500, height: 500), config: config)
+                    outputPNG(url: url, text: "想你的风吹到了\(first)", size: .init(width: 750, height: 400), config: config)
                     outputPNG(url: url, text: "谢谢你", size: .init(width: 750, height: 750), config: config)
                     outputPNG(url: url, text: "你若喜欢 给个赞吧", size: .init(width: 750, height: 560), config: config)
                     let stickListUrl = url.appendingPathComponent("stickerList")
@@ -71,25 +91,31 @@ struct MissYouViewDemo: View {
     ) {
         let fileUrl = url.appendingPathComponent("\(text).png")
         let isSuccess = OutputImg.outputPNG(url: fileUrl) {
-            ZStack {
-                Color.white
-                config.titleBgColor
-                    .opacity(0.8)
-                    .overlay {
-                        Text(text)
-                            .foregroundStyle(config.titleColor)
-                            .font(.youSheBiaoTiHei(max(size.width, size.height)))
-                            .padding()
-                            .lineLimit(1)
-                            .minimumScaleFactor(0.01)
-                    }
-            }
-            .frame(width: size.width, height: size.height)
+            PNG(text: text, size: size, config: config)
         }
         let fileUrlStr = fileUrl.absoluteString
         print("[outputPNG]: \(fileUrlStr.removingPercentEncoding ?? fileUrlStr) \(isSuccess)")
     }
-
+    
+    @ViewBuilder
+    func PNG(text: String, size: CGSize, config: MissYouView.Config) -> some View {
+        ZStack {
+            self.config.titleBgColor
+            let isTwoLine = text.contains("\n")
+            Text(text)
+                .fontDesign(.monospaced)
+                .font(.system(size: max(size.width, size.height)))
+                .lineLimit(isTwoLine ? 2 : 1)
+                .foregroundStyle(self.config.titleColor)
+                .minimumScaleFactor(0.01)
+                .padding()
+                .multilineTextAlignment(.center)
+                .bold()
+        }
+        .border(self.config.titleColor, width: 2)
+        .frame(width: size.width, height: size.height, alignment: .top)
+    }
+    
     private func outputPNG2(
         url: URL,
         text: String,
@@ -119,9 +145,9 @@ struct MissYouView: View {
                 .frame(height: 60)
                 .overlay {
                     HStack {
-                        Text("我在\(text)很想你")
+                        Text(config.textType ? "我在\(text)很想你" : "想你的风吹到了\(text)")
                             .fontDesign(.monospaced)
-                            .font(.system(size: 26))
+                            .font(.system(size:  22))
                             .bold()
                             .foregroundStyle(self.config.titleColor)
                     }
@@ -132,9 +158,9 @@ struct MissYouView: View {
                     HStack {
                         Spacer()
                         Text("WE ALL MISS YOU")
-                            .fontDesign(.rounded)
+                            .fontDesign(.monospaced)
                             .bold()
-                            .font(.system(size: 18))
+                            .font(.system(size: 16))
                         Spacer()
                     }
                     .padding(.horizontal, 8)
@@ -148,6 +174,7 @@ struct MissYouView: View {
     struct Config {
         let titleColor: Color
         let titleBgColor: Color
+        let textType: Bool = false
         
         init(
             titleColor: Color = Color(253, 253, 150),
