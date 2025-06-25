39fb9156d79fd297995b224da9231895e11167b7
eternity6666 <eternity6666@qq.com>
2025-06-26 01:29:25 +0800

    feat(ChromePlugin): 拦截请求

--- ChromePlugin/injected.js
+++ ChromePlugin/injected.js
@@ -1,3 +1,87 @@
+function httpRequest(stickerID, headers = {}) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    const url =
+      "/cgi-bin/mmemoticon-bin/BindRedPacketCover?f=json&stickerid=" +
+      stickerID +
+      "&cover_url=https:%2F%2Fsupport.weixin.qq.com%2Fcgi-bin%2Fmmsupport-bin%2Fshowredpacket%3Fpurchase_token%3DNPC_fTlZVt3WMTC%26is_sell%3D1&req_type=1";
+
+    xhr.open("GET", url, true); // 第三个参数 true 表示异步
+
+    // 设置请求头
+    for (const [key, value] of Object.entries(headers)) {
+      xhr.setRequestHeader(key, value);
+    }
+
+    xhr.onload = function () {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        resolve({
+          status: xhr.status,
+          data: xhr.responseText,
+        });
+      } else {
+        reject({
+          status: xhr.status,
+          statusText: xhr.statusText,
+        });
+      }
+    };
+
+    xhr.onerror = function () {
+      reject({
+        status: xhr.status,
+        statusText: "网络请求失败",
+      });
+    };
+
+    xhr.ontimeout = function () {
+      reject({
+        status: 408,
+        statusText: "请求超时",
+      });
+    };
+
+    xhr.send();
+  });
+}
+
+function trySendUpdateRedCover(response) {
+  const KEY = "trySendUpdateRedCover";
+  function isIn(stickerID) {
+    return (localStorage.getItem(KEY) ?? "").split(",").includes(stickerID);
+  }
+  function putIn(stickerID) {
+    var list = (localStorage.getItem(KEY) ?? "").split(",");
+    list.push(stickerID);
+    localStorage.setItem(KEY, list);
+  }
+  const json = JSON.parse(response);
+  if (json && json.List) {
+    const list = json.List;
+    list.forEach((stickerItem) => {
+      const { StikerID, Status, TotalDownloadNum, TotalSendNum, Name } =
+        stickerItem;
+      if (Status === 7) {
+        if (!isIn(StikerID)) {
+          const requestLog = `request: ${Name} ${StikerID} download=${TotalDownloadNum} send=${TotalSendNum}`;
+          console.log(requestLog);
+          httpRequest(StikerID)
+            .then((response) => {
+              if (response.data === '{"base_resp":{"ret":0,"err_msg":"ok"}}') {
+                if (!isIn(StikerID)) {
+                  putIn(StikerID);
+                }
+              }
+            })
+            .catch((error) => {
+              console.log(StikerID, error);
+            });
+        }
+      }
+    });
+  }
+}
+
 (function (xhr) {
   var XHR = xhr.prototype;
   var open = XHR.open;
@@ -46,9 +130,9 @@
 
   XHR.send = function (postData) {
     var myUrl = this._url ? this._url.toLowerCase() : this._url;
-    console.log("load inject start:", myUrl);
-    console.log("originArguments:", arguments[0]);
-    console.log("originArguments dataType: ", typeof arguments[0]);
+    // console.log("load inject start:", myUrl);
+    // console.log("originArguments:", arguments[0]);
+    // console.log("originArguments dataType: ", typeof arguments[0]);
     if (
       myUrl &&
       myUrl.includes(
@@ -64,14 +148,19 @@
       if (myUrl) {
         // here you get the RESPONSE HEADERS
         var responseHeaders = this.getAllResponseHeaders();
-        console.log("responseHeaders:", responseHeaders);
+        // console.log("responseHeaders:", responseHeaders);
 
         if (this.responseType != "blob" && this.responseText) {
           // responseText is string or null
           try {
+            if (
+              myUrl.includes("/cgi-bin/mmemoticon-bin/home?lang=zh_cn&f=json")
+            ) {
+              trySendUpdateRedCover(this.responseText);
+            }
             // here you get RESPONSE TEXT (BODY), in JSON format, so you can use JSON.parse
             // printing url, request headers, response headers, response body, to console
-            console.log("originResponseText:", this.responseText);
+            // console.log("originResponseText:", this.responseText);
             // https://sticker.weixin.qq.com/cgi-bin/mmemoticon-bin/stikerpage
           } catch (err) {
             console.log("Error in responseType try catch", err);
